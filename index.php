63c18f4ba262c02e2c0c08f0dcb58cf6ae7b8563
Hector <hjavierog@gmail.com>
2013-06-08 20:22:24 +0200

    PHPStorm data.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Héctor
+ * Date: 7/06/13
+ * Time: 11:07
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Furgowea.";
+
+// throw new Exception ("Hey this is a mistake.");
